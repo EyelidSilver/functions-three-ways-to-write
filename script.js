f1a77516245279c4aaebe48ea718f395346bdd1f
Aita K <67955300+EyelidSilver@users.noreply.github.com>
2021-03-02 19:49:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+function squareAddSquare1(num1, num2) {
+return ((num1 * num1) + (num2 * num2)) * ((num1 * num1) + (num2 * num2));
+}
+
+console.log(squareAddSquare1(2, 2))
+
+const squareAddSquare2 = function (num1, num2) {
+return ((num1 * num1) + (num2 * num2)) * ((num1 * num1) + (num2 * num2));
+}
+
+console.log(squareAddSquare2(2, 2))
+
+const squareAddSquare3 = (num1, num2) => {
+return ((num1 * num1) + (num2 * num2)) * ((num1 * num1) + (num2 * num2));
+}
+
+console.log(squareAddSquare3(2, 2))
+
